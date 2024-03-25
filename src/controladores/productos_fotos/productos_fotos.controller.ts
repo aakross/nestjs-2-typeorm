061c8cf5b58f7e0fe8fabefc1aa18d377b96086d
aakroos <royal9619@gmail.com>
2024-03-24 22:40:21 -0600

    24/03/24

--- src/controladores/productos_fotos/productos_fotos.controller.ts
+++ src/controladores/productos_fotos/productos_fotos.controller.ts
@@ -1,5 +1,23 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, FileTypeValidator, Get, MaxFileSizeValidator, Param, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ProductosFotosService } from 'src/servicios/productos_fotos/productos_fotos.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { Producto } from 'src/modelos/productos.entity';
+
+export class SampleDto {
+    producto_id: Producto;
+}
+export const fecha = Date.now();
+
+export const storage = diskStorage({
+    destination: './assets/uploads/productos',
+    filename: (req, file, callback) => {
+        let nombre = `${fecha}${extname(file.originalname)}`;
+        callback(null, nombre);
+    },
+});
+
 
 @Controller('productos-fotos')
 export class ProductosFotosController {
@@ -9,8 +27,19 @@ export class ProductosFotosController {
         return this.productosFotosService.getDatosPorProducto(params.id)
     }
     @Post()
-    metoddoPost()
-    {
-
+    @UseInterceptors(FileInterceptor('file', { storage }))
+    metoddoPost(@Body() dto: SampleDto, @UploadedFile(
+        new ParseFilePipe(
+            {
+                validators:
+                    [
+                        new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+                        new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4 }),
+                    ]
+            }),
+    ) file: Express.Multer.File) {
+        let nombre = `${fecha}${extname(file.originalname)}`;
+        this.productosFotosService.addDatos({ foto: nombre, producto_id: dto.producto_id });
+        return { estado: 'ok', mensaje: "Se creo el registro exitosamente" }
     }
 }
